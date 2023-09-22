0bf943c9490a3a381a3df4673dbda95f9375b3d0
Imam  Taufiq Hermawan <imamtaufiq133@gmail.com>
2023-09-22 21:57:09 +0700

    finish refactor better file structure

---
+++ routes/tourRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const tourController = require("../controllers/toursController")
+
+const router = express.Router()
+
+router.param("id", tourController.checkId)
+
+router
+  .route("/")
+  .get(tourController.getAllTours)
+  .post(
+    tourController.checkBody,
+    tourController.createTour
+  )
+
+router
+  .route("/:id")
+  .get(tourController.getTourById)
+  .patch(tourController.editTour)
+  .delete(tourController.removeTour)
+
+module.exports = router
